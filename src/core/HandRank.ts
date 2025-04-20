7433c3c7528176712faaceb170187d860e0d9c25
Daniel Vlassov <danielvlassov@gmail.com>
2025-04-20 19:57:43 -0400

    Hand Rank fixes

--- src/core/HandRank.ts
+++ src/core/HandRank.ts
@@ -2,9 +2,9 @@ export enum HandRank {
     HighCard = 1,
     Pair,
     TwoPair,
-    ThreeKind,
     Straight,
     Flush,
+    ThreeKind,
     StraightFlush,
     FourKind,
     FourAces, // special for super‑bonus 200 : 1
